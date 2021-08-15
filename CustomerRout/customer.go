8a8b89c52a5797534b1bacbb693ee1af0125ae22
Dilshan Rajika <dilshanrajitha1998@gmail.com>
2021-08-15 12:00:09 +0530

    encode decode apply

--- CustomerRout/customer.go
+++ CustomerRout/customer.go
@@ -5,11 +5,13 @@ import (
 	//Array and slice values encode as JSON arrays
 	"encoding/json"
 	//This is import to write values to the console
-	"fmt"
+
 	// This implements a request router and dispatcher for matching  incoming requests to their
 	"github.com/gorilla/mux"
 	//This gives us HTTP client and server implementation for building the actual API
 	"net/http"
+
+	"encoding/base64"
 )
 
 var customers []customer //Create a Customer Array
@@ -18,7 +20,7 @@ type customer struct {
 	ID      string  `json:"id"`
 	Name    string  `json:"name"`
 	Address string  `json:"address"`
-	Salary  float32 `json:"salary"`
+	Salary  float64 `json:"salary"`
 }
 
 /*------------------------------Save Customer----------------------------------------------------*/
@@ -28,13 +30,13 @@ func SaveCustomer(w http.ResponseWriter, r *http.Request) {
 	if (*r).Method == "OPTIONS" {
 		return
 	}
-	id := r.FormValue("id")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
+	id := base64.StdEncoding.EncodeToString([]byte(r.FormValue("id")))
+	name := base64.StdEncoding.EncodeToString([]byte(r.FormValue("name")))
+	address := base64.StdEncoding.EncodeToString([]byte(r.FormValue("address")))
 	salary := r.FormValue("salary")
-	fmt.Println(salary)
+
 	//get connection
-	db:=DB.GetConnection()
+	db := DB.GetConnection()
 	insert, err := db.Query("INSERT INTO customer VALUES (?, ?, ?, ?)", id, name, address, salary)
 	if err != nil {
 		panic(err.Error())
@@ -50,9 +52,9 @@ func DeleteCustomer(w http.ResponseWriter, r *http.Request) {
 	if (*r).Method == "OPTIONS" {
 		return
 	}
-	id := r.FormValue("id")
+	id := base64.StdEncoding.EncodeToString([]byte(r.FormValue("id")))
 	//get connection
-	db:=DB.GetConnection()
+	db := DB.GetConnection()
 	delete, err := db.Query("delete from customer where id=?", id)
 	if err != nil {
 		panic(err.Error())
@@ -71,13 +73,13 @@ func UpdateCustomer(w http.ResponseWriter, r *http.Request) {
 	if (*r).Method == "OPTIONS" {
 		return
 	}
-	id := r.FormValue("id")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
+	id := base64.StdEncoding.EncodeToString([]byte(r.FormValue("id")))
+	name := base64.StdEncoding.EncodeToString([]byte(r.FormValue("name")))
+	address := base64.StdEncoding.EncodeToString([]byte(r.FormValue("address")))
 	salary := r.FormValue("salary")
 
 	//get connection
-	db:=DB.GetConnection()
+	db := DB.GetConnection()
 	insert, err := db.Query("UPDATE customer set name=?,address=?,salary=? where id=?", name, address, salary, id)
 	if err != nil {
 		panic(err.Error())
@@ -98,7 +100,7 @@ func AllCustomer(w http.ResponseWriter, r *http.Request) {
 	//set header content type application json
 	w.Header().Set("Content-Type", "application/json")
 	//get connection
-	db:=DB.GetConnection()
+	db := DB.GetConnection()
 	//sql get all customer
 	row, err := db.Query("Select * from customer")
 	if err != nil {
@@ -109,19 +111,25 @@ func AllCustomer(w http.ResponseWriter, r *http.Request) {
 			var id string
 			var name string
 			var address string
-			var salary float32
+			var salary float64
 
 			err2 := row.Scan(&id, &name, &address, &salary)
+
 			row.Columns()
 			if err2 != nil {
 				panic(err2.Error())
 			} else {
+				id1, _ := base64.StdEncoding.DecodeString(id)
+				name1, _ := base64.StdEncoding.DecodeString(name)
+				address1, _ := base64.StdEncoding.DecodeString(address)
+				salary1 := salary
+
 				// Initializing Customer struct
 				custList := customer{
-					ID:      id,
-					Name:    name,
-					Address: address,
-					Salary:  salary,
+					ID:      string(id1),
+					Name:    string(name1),
+					Address: string(address1),
+					Salary:  salary1,
 				}
 				//assign cuttList to allCustomer array
 				allCustomer = append(allCustomer, custList)
@@ -131,6 +139,7 @@ func AllCustomer(w http.ResponseWriter, r *http.Request) {
 	//close the execution
 	defer row.Close()
 	//return response
+
 	json.NewEncoder(w).Encode(allCustomer)
 
 }
@@ -148,8 +157,10 @@ func SearchCustomer(w http.ResponseWriter, r *http.Request) {
 	//Query strings in Go are accessed
 	params := mux.Vars(r)
 	//get connection
-	db:=DB.GetConnection()
-	row, err := db.Query("select * from customer where id=?", params["id"])
+	db := DB.GetConnection()
+	id := params["id"]
+	id1 := base64.StdEncoding.EncodeToString([]byte(id))
+	row, err := db.Query("select * from customer where id=?", id1)
 	if err != nil {
 		panic(err.Error())
 	} else {
@@ -157,7 +168,7 @@ func SearchCustomer(w http.ResponseWriter, r *http.Request) {
 			var id string
 			var name string
 			var address string
-			var salary float32
+			var salary float64
 
 			err2 := row.Scan(&id, &name, &address, &salary)
 			row.Columns()
@@ -165,11 +176,15 @@ func SearchCustomer(w http.ResponseWriter, r *http.Request) {
 				panic(err2.Error())
 			} else {
 				// Initializing Customer struct
+				id1, _ := base64.StdEncoding.DecodeString(id)
+				name1, _ := base64.StdEncoding.DecodeString(name)
+				address1, _ := base64.StdEncoding.DecodeString(address)
+				salary1 := salary
 				customer := customer{
-					ID:      id,
-					Name:    name,
-					Address: address,
-					Salary:  salary,
+					ID:      string(id1),
+					Name:    string(name1),
+					Address: string(address1),
+					Salary:  salary1,
 				}
 				cust = customer
 			}
